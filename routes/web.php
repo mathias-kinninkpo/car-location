23effda5c00cdc09466eddb99d2c2cb6caa65f1e
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-01-03 14:07:37 +0100

    les routes et les controlers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// routes/web.php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RentalController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
+
+// Page d'accueil
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Routes pour les voitures
+Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+Route::get('/cars/{car}', [CarController::class, 'show'])->name('cars.show');
+
+// Routes pour les individus
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+
+// Routes pour les locations
+Route::get('/rentals', [RentalController::class, 'index'])->name('rentals.index');
+Route::get('/rentals/create', [RentalController::class, 'create'])->name('rentals.create');
+Route::post('/rentals', [RentalController::class, 'store'])->name('rentals.store');
+
+
+// Routes pour l'authentification et les profils
+Route::get('/register', [RegisterController::class, 'create'])->name('register.create');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+
+Route::get('/login', [LoginController::class, 'create'])->name('login.create');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
+
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+
+
