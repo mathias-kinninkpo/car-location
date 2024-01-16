c7f017019bf34717eefcedf686889c65d6f566c3
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-01-16 16:14:56 +0100

    modification de la page d'accueil

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -8,75 +8,176 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Ajoutez votre fichier CSS personnalisé -->
     <link rel="stylesheet" href="css/index.css">
+    
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top header color-bg">
-        <div class="d-flex align-items-center m-auto" style="width: 80%">
-            <div style="width: 80%">
-                <a class="navbar-brand text-white" href="{{ route('home') }}">CAR LOCATION</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+  @php
+    $isHomePage = request()->is('/');
+  @endphp
+    <header id="header" class="fixed-top d-flex align-items-center {{ $isHomePage ? 'header-transparent' : '' }}">
+        <div class="container d-flex justify-content-between align-items-center">
+    
+            <div class="logo">
+                <a href="{{ route('home') }}" class="text-light">
+                    <span>CAR LOCATION</span>
+                </a>
             </div>
-
-            <div class="w-50 collapse navbar-collapse header-element ms-auto me-auto" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    <li class="nav-item {{ request()->is('/') ? 'active' : '' }}">
-                        <a class="nav-link text-white" href="{{ route('home') }}">Accueil</a>
+    
+            <nav id="navbar" class="navbar">
+                <ul>
+                    <li class="{{ request()->is('/') ? 'active' : '' }}">
+                        <a href="{{ route('home') }}">Accueil</a>
                     </li>
-                    <li class="nav-item {{ request()->is('cars*') ? 'active' : '' }}">
-                        <a class="nav-link text-white" href="{{ route('cars.index') }}">Voitures</a>
+                    <li class="{{ request()->is('cars*') ? 'active' : '' }}">
+                        <a href="{{ route('cars.index') }}">Voitures</a>
                     </li>
-                    <li class="nav-item {{ request()->is('contact') ? 'active' : '' }}">
-                        <a class="nav-link text-white" href="{{ route('contact.index') }}">Contact</a>
+                    <li class="{{ request()->is('contact') ? 'active' : '' }}">
+                        <a href="{{ route('contact.index') }}">Contact</a>
                     </li>
-                    <li class="nav-item {{ request()->is('register*') ? 'active' : '' }}">
-                        <a class="nav-link text-white" href="{{ route('register.index') }}">Inscription</a>
+                    <li class="{{ request()->is('register*') ? 'active' : '' }}">
+                        <a href="{{ route('register.index') }}">Inscription</a>
                     </li>
-                    <li class="nav-item {{ request()->is('login*') ? 'active' : '' }}">
-                        <a class="nav-link text-white" href="{{ route('login.index') }}">Connexion</a>
+                    <li class="{{ request()->is('login*') ? 'active' : '' }}">
+                        <a href="{{ route('login.index') }}">Connexion</a>
                     </li>
                     @auth
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle text-white {{ request()->is('users*') ? 'active' : '' }}" href="{{ route('users.index') }}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Profil
+                        <li class="dropdown">
+                            <a href="{{ route('users.index') }}" class="active">
+                                Profil <i class="bi bi-chevron-down"></i>
                             </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="#">Voir</a>
-                                <a class="dropdown-item" href="#">Éditer</a>
-                            </div>
+                            <ul>
+                                <li><a href="#">Voir</a></li>
+                                <li><a href="#">Éditer</a></li>
+                            </ul>
                         </li>
                     @endauth
                 </ul>
-            </div>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            </nav><!-- .navbar -->
+    
         </div>
-    </nav>
-
+    </header><!-- End Header -->
+    
     <div class="w-100 mt-0">
         @yield('content')
     </div>
 
-    <!-- Footer -->
-    <footer class="footer mt-auto py-5 text-white footer-bg">
-        <div class="container text-center">
-            <span class="text-wite">© 2024 CarLocation - Tous droits réservés</span>
-        </div>
-    </footer>
+    
+  <!-- ======= Footer ======= -->
+  <footer id="footer" data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="500">
 
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 50) {
-                    $('.header').addClass('header-scrolled');
-                } else {
-                    $('.header').removeClass('header-scrolled');
-                }
-            });
-        });
-    </script>
+    <div class="footer-newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h4>Notre Newsletter</h4>
+                    <p>Restez informé sur nos offres spéciales et nos nouvelles voitures.</p>
+                </div>
+                <div class="col-lg-6">
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="S'abonner">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Liens Utiles</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{ route('home') }}">Accueil</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="{{ route('contact.index') }}">Contectez-nous</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Conditions d'utilisation</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Politique de confidentialité</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Nos Services</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Location de Voitures</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Gestion de Flotte</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Réservation en Ligne</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Assistance Client</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Design Graphique</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h4>Contactez-nous</h4>
+                    <p>
+                        Résidence Universitaire <br>
+                        Abomey-Calavi Campus<br>
+                        Bénin - Atlantique <br><br>
+                        <strong>Téléphone :</strong> +229 62396632<br>
+                        <strong>Email :</strong> info@carlocation.com<br>
+                    </p>
+
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-info">
+                    <h3>À Propos de Car Location</h3>
+                    <p>Car Location s'engage à fournir un service de location de voitures de qualité pour rendre vos déplacements plus faciles et plus confortables.</p>
+                    <div class="social-links mt-3">
+                        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong><span>Car Location</span></strong>. Tous droits réservés
+        </div>
+        <div class="credits">
+            Conçu par <a href="https://github.com/mathias-kinninkpo">Mathias KINNINKPO</a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,195 @@
 @extends('base')
 
 @section('content')
-    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active custom-carousel-item">
-                <img src="assets/cartwo.png" class="d-block w-100" alt="Image 1" style="height: 100vh">
-                <div class="carousel-caption d-none d-md-flex flex-column align-items-center text-center text-white" style="height:65%">
-                    <h1>Bienvenue sur CarLocation</h1>
-                    <h3>La meilleure destination pour la location de voitures.</h3>
-                    <hr class="my-4">
-                    <h3>Explorez notre flotte de voitures et trouvez celle qui correspond à vos besoins.</h3>
-                    <a class="btn btn-success btn-lg" href="{{ route('cars.index') }}" role="button">Voir les Voitures</a>
-                </div>
-            </div>
-            <div class="carousel-item custom-carousel-item">
-                <img src="assets/carsix.jpeg" class="d-block w-100" alt="Image 2" style="height: 100vh">
-                <div class="carousel-caption d-none d-md-flex flex-column align-items-center text-center text-white" style="height:65%">
-                    <h1>Découvrez notre sélection</h1>
-                    <h3>Des voitures de qualité pour une expérience de conduite exceptionnelle.</h3>
-                    <hr class="my-4">
-                    <h3>Parcourez notre collection et trouvez la voiture parfaite pour chaque occasion.</h3>
-                    <a class="btn btn-success btn-lg" href="{{ route('cars.index') }}" role="button">Voir les Voitures</a>
-                </div>
-            </div>
-            <div class="carousel-item custom-carousel-item">
-                <img src="assets/carfive.jpeg" class="d-block w-100" alt="Image 3" style="height: 100vh">
-                <div class="carousel-caption d-none d-md-flex flex-column align-items-center text-center text-white" style="height:65%">
-                    <h1>Réservez dès aujourd'hui</h1>
-                    <h3>Profitez de tarifs compétitifs et d'une réservation simple en ligne.</h3>
-                    <hr class="my-4">
-                    <h3>Réservez la voiture de vos rêves en quelques clics.</h3>
-                    <a class="btn btn-success btn-lg" href="{{ route('cars.index') }}" role="button">Voir les Voitures</a>
-                </div>
+    <!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex justify-content-center align-items-center">
+    <div id="heroCarousel" class="container carousel carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active">
+            <div class="carousel-container">
+                <h2 class="animate__animated animate__fadeInDown">Bienvenue chez <span>Car Location</span></h2>
+                <p class="animate__animated animate__fadeInUp">Explorez notre large gamme de voitures à louer. Trouvez la voiture parfaite pour vos besoins de déplacement. Réservez en ligne dès maintenant.</p>
+                <a href="#" class="btn-get-started animate__animated animate__fadeInUp">En savoir plus</a>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-    
 
-    <div class="jumbotron my-4 " style="width: 80%; margin:auto;">
-        <h1 class="display-4">Bienvenue sur CarLocation</h1>
-        <p class="lead">La meilleure destination pour la location de voitures.</p>
-        <hr class="my-4">
-        <p>Explorez notre flotte de voitures et trouvez celle qui correspond à vos besoins.</p>
-        <a class="btn btn-success btn-lg" href="{{ route('cars.index') }}" role="button">Voir les Voitures</a>
+        <!-- Slide 2 -->
+        <div class="carousel-item">
+            <div class="carousel-container">
+                <h2 class="animate__animated animate__fadeInDown">Découvrez le confort de conduire avec Car Location</h2>
+                <p class="animate__animated animate__fadeInUp">Trouvez la voiture idéale qui correspond à votre style et à votre budget. Profitez d'une expérience de conduite sans tracas avec Car Location.</p>
+                <a href="#" class="btn-get-started animate__animated animate__fadeInUp">En savoir plus</a>
+            </div>
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item">
+            <div class="carousel-container">
+                <h2 class="animate__animated animate__fadeInDown">Choix varié, tarifs compétitifs</h2>
+                <p class="animate__animated animate__fadeInUp">Car Location offre une large sélection de voitures de qualité à des tarifs compétitifs. Trouvez la voiture parfaite pour chaque occasion.</p>
+                <a href="#" class="btn-get-started animate__animated animate__fadeInUp">En savoir plus</a>
+            </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon bx bx-chevron-left" aria-hidden="true"></span>
+        </a>
+
+        <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon bx bx-chevron-right" aria-hidden="true"></span>
+        </a>
+
     </div>
+</section><!-- End Hero -->
+
+<main id="main">
+
+    <!-- ======= Services Section ======= -->
+    <section class="services">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up">
+                    <div class="icon-box icon-box-pink">
+                        <div class="icon"><i class="bx bxl-car"></i></div>
+                        <h4 class="title"><a href="#">Large Flotte de Voitures</a></h4>
+                        <p class="description">Découvrez notre large gamme de voitures, des compactes aux SUV, adaptées à tous les types de voyages.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                    <div class="icon-box icon-box-cyan">
+                        <div class="icon"><i class="bx bx-credit-card"></i></div>
+                        <h4 class="title"><a href="#">Réservation en Ligne Facile</a></h4>
+                        <p class="description">Réservez votre voiture en ligne en quelques clics. Paiement sécurisé et confirmation instantanée.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                    <div class="icon-box icon-box-green">
+                        <div class="icon"><i class="bx bx-time"></i></div>
+                        <h4 class="title"><a href="#">Service Rapide</a></h4>
+                        <p class="description">Gagnez du temps avec notre service rapide. Prenez la route rapidement avec Car Location.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                    <div class="icon-box icon-box-blue">
+                        <div class="icon"><i class="bx bx-map"></i></div>
+                        <h4 class="title"><a href="#">Lieux Variés</a></h4>
+                        <p class="description">Avec des emplacements pratiques, Car Location rend la location de voiture facile et accessible.</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <section class="why-us section-bg" data-aos="fade-up" data-aos-delay="200">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-6 video-box">
+                    <img src="assets/img/carfour.jpg" class="img-fluid" alt="">
+                    <a href="https://www.youtube.com/watch?v=UpaBOPwXpmc" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+                </div>
+
+                <div class="col-lg-6 d-flex flex-column justify-content-center p-5">
+
+                    <div class="icon-box">
+                        <div class="icon"><i class="bx bx-fingerprint"></i></div>
+                        <h4 class="title"><a href="#">Qualité et Confiance</a></h4>
+                        <p class="description">Car Location est dédié à offrir une expérience de location de voiture de qualité avec une confiance totale.</p>
+                    </div>
+
+                    <div class="icon-box">
+                        <div class="icon"><i class="bx bx-gift"></i></div>
+                        <h4 class="title"><a href="#">Offres Spéciales</a></h4>
+                        <p class="description">Profitez de nos offres spéciales et réductions pour économiser sur votre prochaine location de voiture.</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Why Us Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section class="features">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Fonctionnalités</h2>
+                <p>Explorez les fonctionnalités qui font de Car Location le choix idéal pour vos besoins de location de voiture.</p>
+            </div>
+
+            <div class="row" data-aos="fade-up">
+                <div class="col-md-5">
+                    <img src="assets/img/features-1.svg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-4">
+                    <h3>Large Sélection de Voitures</h3>
+                    <p class="fst-italic">
+                        Trouvez la voiture parfaite parmi notre vaste gamme de véhicules bien entretenus.
+                    </p>
+                    <ul>
+                        <li><i class="bi bi-check"></i> Des voitures compactes aux SUV spacieux.</li>
+                        <li><i class="bi bi-check"></i> Options pour tous les budgets.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row" data-aos="fade-up">
+                <div class="col-md-5 order-1 order-md-2">
+                    <img src="assets/img/features-2.svg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5 order-2 order-md-1">
+                    <h3>Réservation Facile et Rapide</h3>
+                    <p class="fst-italic">
+                        Réservez votre voiture en ligne en quelques étapes simples.
+                    </p>
+                    <p>
+                        Processus de réservation convivial avec confirmation instantanée.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row" data-aos="fade-up">
+                <div class="col-md-5">
+                    <img src="assets/img/features-3.svg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5">
+                    <h3>Service Client Dédié</h3>
+                    <p>Notre équipe de service client est disponible 24/7 pour répondre à vos questions et vous assister.</p>
+                    <ul>
+                        <li><i class="bi bi-check"></i> Assistance en temps réel.</li>
+                        <li><i class="bi bi-check"></i> Support téléphonique et en ligne.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row" data-aos="fade-up">
+                <div class="col-md-5 order-1 order-md-2">
+                    <img src="assets/img/features-4.svg" class="img-fluid" alt="">
+                </div>
+                <div class="col-md-7 pt-5 order-2 order-md-1">
+                    <h3>Points de Ramassage Pratiques</h3>
+                    <p class="fst-italic">
+                        Retirez et déposez votre voiture à des emplacements pratiques.
+                    </p>
+                    <p>
+                        Emplacements stratégiquement situés pour votre confort.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Features Section -->
+
+</main>
+
+
 @endsection
